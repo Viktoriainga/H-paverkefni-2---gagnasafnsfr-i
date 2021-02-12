a3459acdaaa40cba7b3690b6988f289342d9296b
Astthorarnorsson <astthor19@ru.is>
2021-02-12 11:10:36 +0000

    Lokaskil2

--- Hopverkefni2_queries.sql
+++ Hopverkefni2_queries.sql
@@ -11,11 +11,25 @@ ORDER BY location ASC;
 
 select 3 as Query; --Ingó
 
--- select ...
+SELECT
+    COUNT(*)
+FROM
+    people
+WHERE
+    genderid = (
+        SELECT
+            GenderID
+        FROM
+            Genders
+        WHERE
+            gender = 'Female'
+    );
 
 select 4 as Query; --Viktoría
 
--- select ...
+SELECT LocationID, location
+FROM Locations 
+ORDER BY location DESC;
 
 select 5 as Query; --Ástþór
 
@@ -25,12 +39,21 @@ JOIN Cases C
 ON P.LocationID = C.LocationID
 JOIN InvolvedIn I
 ON P.PersonID = I.PersonID
-WHERE I.isCulprit = TRUE
+WHERE I.isCulprit = TRUE;
 
 
 select 6 as Query; --Ingó
 
--- select ...
+SELECT
+    P.personID,
+    P.name,
+    P.genderID
+FROM
+    People as P
+    INNER JOIN Cases AS C ON C.locationid = 33
+    INNER JOIN Agents AS A on C.agentid = A.agentid
+WHERE
+    A.secretIdentity = P.PersonID;
 
 select 7 as Query; --Viktoría
 
@@ -44,11 +67,29 @@ JOIN Genders G
 ON A.GenderID = G.GenderID
 JOIN Passwords P
 ON A.AgentID = P.AgentID
-WHERE P.password LIKE CONCAT('%',A.codename,'%')
+WHERE P.password LIKE CONCAT('%',A.codename,'%');
 
 select 9 as Query; --Ingó
 
--- select ...
+SELECT
+    DISTINCT P.PersonID,
+    P.name,
+    CASE
+        WHEN (I.isCulprit = true) THEN 'guilty'
+        ELSE 'not guilty'
+    END AS hasBeenCulprit
+FROM
+    People P
+    INNER JOIN InvolvedIN AS I ON I.PersonID = P.PersonID
+    INNER JOIN Cases AS C ON C.CaseID = I.CaseID
+    INNER JOIN Locations AS L ON C.locationid = L.locationid
+GROUP BY
+    P.PersonID,
+    L.LocationID,
+    I.isCulprit
+HAVING
+    COUNT(I.CaseID) >= 2
+    AND L.Location LIKE '%vogur';
 
 select 10 as Query; --Viktoría
 
