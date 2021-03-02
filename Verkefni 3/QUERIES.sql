c32631967535d507cbde294e202d5cc6e41ea842
Astthorarnorsson <astthor19@ru.is>
2021-03-02 14:32:28 +0000

    Update QUERIES.sql

--- Verkefni 3/QUERIES.sql
+++ Verkefni 3/QUERIES.sql
@@ -11,8 +11,16 @@ select 2 as Query; -- Ingo
 select 3 as Query; -- Asi
 
 SELECT A.codename
-FROM Agents A
-INNER JOIN 
+FROM A.Agents
+WHERE
+    secretIdentity IN(
+        SELECT A2.secretIdentity
+        FROM Agents A2
+        NATURAL JOIN InvolvedIn I1
+        INTERSECT
+        SELECT I2.PersonID
+        FROM InvolvedIn I2
+        WHERE I2.isCulprit = TRUE);
 
 
 select 4 as Query; -- Vikta
@@ -25,7 +33,9 @@ select 5 as Query; -- Ingo
 
 select 6 as Query; -- Asi
 
--- select ...
+SELECT A.codename, A.designation
+FROM Agents A
+
 
 select 7 as Query; -- Vikta
 
@@ -37,7 +47,14 @@ select 8 as Query; -- Ingo
 
 select 9 as Query; -- Asi
 
--- select ...
+SELECT C.CaseID, C.title, L.location
+FROM Cases C 
+INNER JOIN Locations L ON C.LocationID = L.LocationID
+
+SELECT I.PersonID
+FROM InvolvedIn I
+INNER JOIN Cases C ON C.CaseID = I.CaseID
+
 
 select 10 as Query; -- Alles
 
