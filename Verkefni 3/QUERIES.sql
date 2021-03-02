38efd763ca76e3f7635c30c9eeb76d0992fb9da5
ingolfurorri <ingolfurorri@gmail.com>
2021-03-02 21:16:11 +0000

    Problem 8 finsihed

--- Verkefni 3/QUERIES.sql
+++ Verkefni 3/QUERIES.sql
@@ -82,6 +82,7 @@ SELECT A.designation, A.codename
 FROM Agents A
 WHERE A.agentID NOT IN (
     SELECT C.agentID
+    FROM Cases C
     INNER JOIN Locations L ON C.LocationID = L.LocationID
     INNER JOIN Agents A on A.agentID = C.agentID
     WHERE L.location = 'Akranes'
