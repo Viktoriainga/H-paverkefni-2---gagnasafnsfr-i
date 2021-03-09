e48106bed0c92a1d4fc27a65f202f5633f2dae0d
viktoriainga <viktoria19@ru.is>
2021-03-09 21:30:07 +0000

    búa til

---
+++ Verkefni 4/CODE.sql
@@ -0,0 +1,23 @@
+-- Authors: Ástþór Arnórsson, Ingólfur Orri Gústafsson og Viktoría Inga Smáradóttir
+CREATE DATABASE PIII
+
+select 1 as Query;  --Asi
+
+select 2 as Query; --Ingo
+
+select 3 as Query; --Vik
+
+select 4 as Query; --Asi
+
+select 5 as Query; --Ingo
+
+select 6 as Query; --Vik
+
+select 7 as Query; --Asi
+
+select 8 as Query; --Ingo
+
+select 9 as Query; --Vik
+
+select 10 as Query; --Allir
+
