f5bf687d2eae206a2fc54c398f8614f0d9c5d40c
Ingólfur Orri <ingolfurorri@gmail.com>
2021-03-28 18:32:19 +0000

    Added mvd_table to generator

--- Verkefni 5/generator.py
+++ Verkefni 5/generator.py
@@ -32,16 +32,21 @@ MVD_template = '''
     ) X;
 '''
 
-tables = [
+pd_tables = [
     ("CivilServices", ("CSID", "HID", "PN", "S", "HS", "HZ", "HC")),
     ("Projects", ("ID", "PID", "SID", "SN", "PN", "MID", "MN")),
     ("Citizens", ("CID", "CN", "CS", "CNr", "CZ", "CL", "EID")),
     ("Coffees", ("DID", "HID", "CID", "DN", "DS", "CN", "CC"))
 ]
 
+mvd_tables = [
+    ("Projects", ("ID", "PID", "SID", "SN", "PN", "MID", "MN")),
+    ("Coffees", ("DID", "HID", "CID", "DN", "DS", "CN", "CC"))
+]
+
 def main_generator_PD():
     with open("prj5_check_FD.sql", "w") as f:
-        for table, attributes in tables:
+        for table, attributes in pd_tables:
             #if table == 'Citizens':
             for a,b in permutations(attributes, 2):
                 query = FD_template.format(R=table, A=a, B=b)
@@ -50,7 +55,7 @@ def main_generator_PD():
 
 def main_generator_MVD():
     with open("prj5_check_MVD.sql", "w") as f:
-        for table, attributes in tables:
+        for table, attributes in mvd_tables:
             #if table == 'Citizens':
             for a,b,c in permutations(attributes, 3):
                 query = MVD_template.format(R=table, A=a, B=b, C=c)
